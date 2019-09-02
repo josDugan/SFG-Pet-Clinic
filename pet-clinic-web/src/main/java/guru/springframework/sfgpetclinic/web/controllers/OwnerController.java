e94b169964adbe1cbe95431a2ddaec3c2b448d3c
josDugan <josephdug@gmail.com>
2019-09-02 17:14:26 -0400

    Implement listing all users on owners list html. Closes #16.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/web/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/web/controllers/OwnerController.java
@@ -1,13 +1,25 @@
 package guru.springframework.sfgpetclinic.web.controllers;
 
+import guru.springframework.sfgpetclinic.data.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "/owners/list", "/owners/index", "/owners/index.html"})
-    public String listOnwers() {
+    public String listOnwers(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/list";
     }
 }
