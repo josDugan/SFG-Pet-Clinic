25c4505b8113fa7effa246dfc7421fb6cfe82886
josDugan <josephdug@gmail.com>
2019-09-16 18:13:27 -0400

    Add PetType bootstrap data. Closes #32

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/web/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/web/bootstrap/DataLoader.java
@@ -1,11 +1,11 @@
 package guru.springframework.sfgpetclinic.web.bootstrap;
 
 import guru.springframework.sfgpetclinic.data.model.Owner;
+import guru.springframework.sfgpetclinic.data.model.PetType;
 import guru.springframework.sfgpetclinic.data.model.Vet;
 import guru.springframework.sfgpetclinic.data.services.OwnerService;
+import guru.springframework.sfgpetclinic.data.services.PetTypeService;
 import guru.springframework.sfgpetclinic.data.services.VetService;
-import guru.springframework.sfgpetclinic.data.services.map.OwnerServiceMap;
-import guru.springframework.sfgpetclinic.data.services.map.VetServiceMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -15,15 +15,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Micheal");
