dadbeeb3b28de1bb0c306cf1f96e2581610f9c1d
josDugan <josephdug@gmail.com>
2019-09-15 20:50:59 -0400

    Add Vet Specialty and associate with Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/data/model/Specialty.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.data.model;
+
+public class Specialty {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
