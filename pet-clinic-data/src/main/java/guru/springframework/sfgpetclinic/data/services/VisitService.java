43e4171cfc02370c455959e172b62e93d8b40532
josDugan <josephdug@gmail.com>
2019-10-02 22:06:33 -0400

    Add Visit map Service. Closes #46.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/data/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.data.services;
+
+import guru.springframework.sfgpetclinic.data.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
