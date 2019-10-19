684f2bb50f3bdeb1d815a721b14d57b4ffb5ec6e
josDugan <josephdug@gmail.com>
2019-10-19 15:06:45 -0400

    Added junit 5 dependencies. Removed junit 4. Closes #4.

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/web/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/web/SfgPetClinicApplicationTests.java
@@ -1,16 +1,16 @@
 package guru.springframework.sfgpetclinic.web;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class SfgPetClinicApplicationTests {
 
-//	@Test
-//	public void contextLoads() {
-//	}
+	@Test
+	public void contextLoads() {
+	}
 
 }
