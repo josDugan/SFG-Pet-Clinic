4326fe8fcbfd054d6e6740a0735c2ce19c8a907c
josDugan <josephdug@gmail.com>
2019-11-17 11:01:43 -0500

    Add display owner partial implementaion. #53

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/web/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/web/controllers/OwnerController.java
@@ -4,8 +4,12 @@ import guru.springframework.sfgpetclinic.data.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
@@ -16,15 +20,22 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"/owners", "/owners/list", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"", "/list", "/index", "/index.html"})
     public String listOnwers(Model model) {
         model.addAttribute("owners", ownerService.findAll());
 
         return "owners/list";
     }
 
-    @RequestMapping("/owners/find")
+    @RequestMapping("/find")
     public String findOwners() {
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
